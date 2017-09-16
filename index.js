23665deb3b5e6a9821c352d6fe6960812a30320c
David Reep <davereep@gmail.com>
2017-09-16 03:33:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 //const app = "I don't do much."
 
-var kittens = ['k1', 'k2', 'k3']
+//var kittens = ['k1', 'k2', 'k3']
 
 
  function destructivelyAppendKitten(name) {
    kittens.push(name)
-   return kittens
  }
 
  function destructivelyPrependKitten(name) {
