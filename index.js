965cff4d1cbb33e6fb039d62bc416596089596e3
David Reep <davereep@gmail.com>
2017-09-16 03:33:42 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,17 +9,14 @@
 
  function destructivelyPrependKitten(name) {
   kittens.unshift(name)
-  return kittens
 }
 
   function destructivelyRemoveLastKitten(name) {
   kittens.pop(name)
-  return kittens
 }
 
   function destructivelyRemoveFirstKitten(name) {
     kittens.shift(name)
-    return kittens
   }
 
   function appendKitten(name) {
