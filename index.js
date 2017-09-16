e637ca80e94e39cd23ee9f2e8dcfe713119382d8
David Reep <davereep@gmail.com>
2017-09-16 03:16:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-const app = "I don't do much."
+//const app = "I don't do much."
+
+kittens = ["k1", "k2", "k3"] 
+
+
+ function destructivelyAppendKitten(name) {
+   kittens.pop(kittens.length -1)
+   return kittens
+ }
